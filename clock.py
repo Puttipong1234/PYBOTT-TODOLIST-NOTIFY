dbd604d559bfa2d4a2aa2ee337b565873c9f90d0
Puttipong1234 <Puttipong.lims@gmail.com>
2020-08-28 18:26:28 +0700

    add

--- clock.py
+++ clock.py
@@ -14,34 +14,34 @@ from datetime import datetime
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=3) # เตือนทุกสิบโมง จันถึงศุกร์ (heroku server +7 GMT thailand)
 def notify_app():
     
-    data_to_noti = "📋 TODOLIST .... ประจำวันที่ {} 📋".format(str(datetime.now())[:10])
+    data_to_noti = "\n📋 TODOLIST .... ประจำวันที่ {} 📋".format(str(datetime.now())[:10])
     r = requests.post(url, headers=headers , data = {'message':data_to_noti})
     
     msgs = get_noti_data()
     for msg in msgs:
         r = requests.post(url, headers=headers , data = {'message':msg})
     
-    break_line = "📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋📋"
+    break_line = "\n🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥"
     r = requests.post(url, headers=headers , data = {'message':break_line})
 
-# @sched.scheduled_job('interval', seconds=30)
-# def TEST_CRON_NOTIFY():
-#     """
-#     Main cron job.
-#     The main cronjob to be run continuously.
-#     """
-#     print("Cron job is running")
-#     print("Tick! The time is: %s" % datetime.now())
+@sched.scheduled_job('interval', seconds=600)
+def TEST_CRON_NOTIFY():
+    """
+    Main cron job.
+    The main cronjob to be run continuously.
+    """
+    print("Cron job is running")
+    print("Tick! The time is: %s" % datetime.now())
     
-#     data_to_noti = "📋ทดสอบทุกๆ 30 วินาที TODOLIST .... ประจำวันที่ {} 📋".format(str(datetime.now())[:10])
-#     r = requests.post(url, headers=headers , data = {'message':data_to_noti})
+    data_to_noti = "\n📋 TODOLIST .... ประจำวันที่ {} 📋".format(str(datetime.now())[:10])
+    r = requests.post(url, headers=headers , data = {'message':data_to_noti})
     
-#     msgs = get_noti_data()
-#     for msg in msgs:
-#         r = requests.post(url, headers=headers , data = {'message':msg})
+    msgs = get_noti_data()
+    for msg in msgs:
+        r = requests.post(url, headers=headers , data = {'message':msg})
     
-#     break_line = "📋 Thank For Your Kind Attention ! 📋"
-#     r = requests.post(url, headers=headers , data = {'message':break_line})
+    break_line = "\n🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥"
+    r = requests.post(url, headers=headers , data = {'message':break_line})
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
