246cf1d549308ca66ed565a120b3689f17144070
Puttipong1234 <Puttipong.lims@gmail.com>
2020-08-28 11:59:47 +0700

    add

--- clock.py
+++ clock.py
@@ -2,6 +2,17 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
+from datetime import datetime
+
+@sched.scheduled_job('interval', seconds=30)
+def cronjob():
+    """
+    Main cron job.
+    The main cronjob to be run continuously.
+    """
+    print("Cron job is running")
+    print("Tick! The time is: %s" % datetime.now())
+
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
     print('This job is run every three minutes.')
